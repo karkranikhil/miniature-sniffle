c37fe34a4e70409e2370ebe4604a6e6671f17f48
jaiswalSomi <somi.jaiswal@rxpservices.com>
2021-01-17 23:53:03 +1100

    job board project using lwc

--- src/client/modules/lib/lightningElementWithBootstrap.js
+++ src/client/modules/lib/lightningElementWithBootstrap.js
@@ -2,7 +2,7 @@ import { LightningElement } from "lwc";
 export default class LightningElementWithBootstrap extends LightningElement {
     constructor(){
         super()
-        const path = '/resources/css/bootstrap.min.css'
+        const path = './resources/css/bootstrap.min.css'
         const styles = document.createElement('link')
         styles.href=path
         styles.rel="stylesheet"
